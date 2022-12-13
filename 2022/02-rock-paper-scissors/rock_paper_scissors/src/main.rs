dcf26dee9a0e5b019b9453f58c9079742b4180fc
Yslan Ramos <yslanramos.r@gmail.com>
2022-12-13 03:08:07 -0300

    2020 day 2

--- 2022/02-rock-paper-scissors/rock_paper_scissors/src/main.rs
+++ 2022/02-rock-paper-scissors/rock_paper_scissors/src/main.rs
@@ -1,3 +1,148 @@
+use std::str::FromStr;
+use std::{env, fs};
+
+enum BattleResult {
+    Win,
+    Wrong,
+    Draw,
+}
+
+impl BattleResult {
+    pub fn score(self) -> i8 {
+        match self {
+            Self::Win => 6,
+            Self::Draw => 3,
+            Self::Wrong => 0,
+        }
+    }
+
+    pub fn battle(user: RockPaperScissor, enemy: RockPaperScissor) -> Self {
+        match (user, enemy) {
+            (RockPaperScissor::Paper, RockPaperScissor::Rock)
+            | (RockPaperScissor::Rock, RockPaperScissor::Scissors)
+            | (RockPaperScissor::Scissors, RockPaperScissor::Paper) => Self::Win,
+            (x, y) if x == y => Self::Draw,
+            _ => Self::Wrong,
+        }
+    }
+
+    pub fn convert_to_part_2_rule(user: RockPaperScissor) -> Self {
+        match user {
+            RockPaperScissor::Rock => Self::Wrong,
+            RockPaperScissor::Paper => Self::Draw,
+            RockPaperScissor::Scissors => Self::Win,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq)]
+enum RockPaperScissor {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl RockPaperScissor {
+    fn score(self) -> i8 {
+        match self {
+            Self::Rock => 1,
+            Self::Paper => 2,
+            Self::Scissors => 3,
+        }
+    }
+
+    fn weak_against(self) -> Self {
+        match self {
+            Self::Paper => Self::Scissors,
+            Self::Rock => Self::Paper,
+            Self::Scissors => Self::Rock,
+        }
+    }
+
+    fn strong_against(self) -> Self {
+        match self {
+            Self::Paper => Self::Rock,
+            Self::Rock => Self::Scissors,
+            Self::Scissors => Self::Paper,
+        }
+    }
+
+    fn battle_with(user: Self, enemy: Self) -> i64 {
+        let result = BattleResult::battle(user, enemy).score();
+        (result + user.score()) as i64
+    }
+
+    fn battle_with_part_2_rules(user: Self, enemy: Self) -> i64 {
+        let user = match BattleResult::convert_to_part_2_rule(user) {
+            BattleResult::Draw => enemy,
+            BattleResult::Win => enemy.weak_against(),
+            BattleResult::Wrong => enemy.strong_against(),
+        };
+
+        Self::battle_with(user, enemy)
+    }
+
+    pub fn battle_with_part_1_rules_from_str(contents: String) -> i64 {
+        contents
+            .lines()
+            .map(|l| {
+                let data: Vec<Self> = l.split(' ').map(|v| Self::from_str(v).unwrap()).collect();
+                Self::battle_with(data[1], data[0])
+            })
+            .sum::<i64>()
+    }
+
+    pub fn battle_with_part_2_rules_from_str(contents: String) -> i64 {
+        contents
+            .lines()
+            .map(|l| {
+                let data: Vec<Self> = l.split(' ').map(|v| Self::from_str(v).unwrap()).collect();
+                Self::battle_with_part_2_rules(data[1], data[0])
+            })
+            .sum::<i64>()
+    }
+}
+
+impl FromStr for RockPaperScissor {
+    type Err = ();
+
+    fn from_str(str: &str) -> Result<Self, ()> {
+        match str {
+            "A" | "X" => Ok(Self::Rock),
+            "B" | "Y" => Ok(Self::Paper),
+            "C" | "Z" => Ok(Self::Scissors),
+            _ => Err(()),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let file_path = env::args().nth(1).unwrap_or("input.txt".to_string());
+    let contents = fs::read_to_string(file_path).unwrap();
+    let data = RockPaperScissor::battle_with_part_2_rules_from_str(contents);
+
+    println!("Hello, world! {data:?}");
+}
+
+#[cfg(test)]
+mod rock_paper_scissors_tests {
+    use crate::RockPaperScissor;
+
+    #[test]
+    fn battle_with_part_1_rules_from_str() {
+        let input = "A Y\nB X\nC Z".to_string();
+        assert_eq!(
+            RockPaperScissor::battle_with_part_1_rules_from_str(input),
+            15
+        );
+    }
+
+    #[test]
+    fn battle_with_part_2_rules_from_str() {
+        let input = "A Y\nB X\nC Z".to_string();
+        assert_eq!(
+            RockPaperScissor::battle_with_part_2_rules_from_str(input),
+            12
+        );
+    }
 }
