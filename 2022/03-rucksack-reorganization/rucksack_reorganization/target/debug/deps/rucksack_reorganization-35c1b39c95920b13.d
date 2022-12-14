b40339e718cf5d8ba1fc7d78747afffa1780f6e6
Yslan Ramos <yslanramos.r@gmail.com>
2022-12-13 22:13:03 -0300

    fix: delete build folder

--- 2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-35c1b39c95920b13.d
+++
@@ -1,5 +0,0 @@
-/home/yslan/dev/advert-of-code/2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-35c1b39c95920b13.rmeta: src/main.rs
-
-/home/yslan/dev/advert-of-code/2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-35c1b39c95920b13.d: src/main.rs
-
-src/main.rs:
--- 2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-87c27802dbeb52db.d
+++
@@ -1,5 +0,0 @@
-/home/yslan/dev/advert-of-code/2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-87c27802dbeb52db.rmeta: src/main.rs
-
-/home/yslan/dev/advert-of-code/2022/03-rucksack-reorganization/rucksack_reorganization/target/debug/deps/rucksack_reorganization-87c27802dbeb52db.d: src/main.rs
-
-src/main.rs:
